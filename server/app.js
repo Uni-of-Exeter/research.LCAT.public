2d7e6ee0b6279c76bf1a80a2db2a23e461f074db
Simon Kirby <s.kirby@exeter.ac.uk>
2024-06-04 12:08:11 +0100

    fix(router): Serve files in server/public.

--- server/app.js
+++ server/app.js
@@ -22,6 +22,7 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', apiRouter);
 
 // Serve static files from the client build directory
