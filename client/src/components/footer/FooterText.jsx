f3dc33f6179f694c48f885efa2cc0c363b0c21b9
Simon Kirby <s.kirby@exeter.ac.uk>
2024-06-04 15:48:45 +0100

    Update link to source.

--- client/src/components/footer/FooterText.jsx
+++ client/src/components/footer/FooterText.jsx
@@ -43,7 +43,7 @@ function FooterText() {
             </p>
 
             <p>
-                <a href="https://github.com/UniExeterRSE/LCAT" target="_blank" rel="noreferrer">
+                <a href="https://github.com/Uni-of-Exeter/research.LCAT.public" target="_blank" rel="noreferrer">
                     Source code published
                 </a>{" "}
                 open source under the{" "}
