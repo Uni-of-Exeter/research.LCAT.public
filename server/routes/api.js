cc99f07ae00792fdc1f83d8109194cff41fa80eb
Simon Kirby <s.kirby@exeter.ac.uk>
2024-06-05 15:46:31 +0100

    fix: add updated license to server

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,3 +1,15 @@
+/* Development before 2024 Copyright (C) Then Try This and University of Exeter
+Development from 2024 Copyright (C) University of Exeter
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the Common Good Public License Beta 1.0 as
+published at http://www.cgpl.org
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+Common Good Public License Beta 1.0 for more details. */
+
 var express = require('express');
 var router = express.Router();
 
