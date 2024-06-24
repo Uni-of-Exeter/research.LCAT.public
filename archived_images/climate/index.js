9d030c1186cb87c264de9238e667d515bf69c529
Simon Kirby <s.kirby@exeter.ac.uk>
2024-06-24 16:49:04 +0100

    Remove archived image folder.

--- archived_images/climate/index.js
+++
@@ -1,4 +0,0 @@
-export { default as CloudCover } from "./CloudCover";
-export { default as Rain } from "./Rain";
-export { default as Temperature } from "./Temperature";
-export { default as WindSpeed } from "./WindSpeed";
