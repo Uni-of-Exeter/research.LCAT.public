38f833f0130c08318b22cf81354e6cc944594c6c
Simon Kirby <s.kirby@exeter.ac.uk>
2024-12-11 10:32:31 +0000

    refactor: change App to functional component and manage state with hooks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ Common Good Public License Beta 1.0 for more details. */
 
 import "./App.css";
 
-import React from "react";
+import React, { useState } from "react";
 
 import StaticAdaptations from "./components/adaptations/StaticAdaptations";
 import ClimateHazardRisk from "./components/climateHazard/ClimateHazardRisk";
@@ -28,163 +28,117 @@ import LCATHeader from "./components/header/Header";
 import Introduction from "./components/header/Introduction";
 import ClimatePredictionLoader from "./components/loaders/ClimatePredictionLoader";
 import PersonalSocialVulnerabilities from "./components/vulnerabilities/PersonalSocialVulnerabilities";
+import { defaultState } from "./utils/defaultState";
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
+function App() {
+    const [regions, setRegions] = useState(defaultState.regions);
+    const [regionType, setRegionType] = useState(defaultState.regionType);
+    const [climatePrediction, setClimatePrediction] = useState(defaultState.climatePrediction);
+    const [season, setSeason] = useState(defaultState.season);
+    const [rcp, setRcp] = useState(defaultState.rcp);
+    const [year, setYear] = useState(defaultState.year);
+    const [loadingPrediction, setLoadingPrediction] = useState(defaultState.loadingPrediction);
+    const [selectedHazardName, setSelectedHazardName] = useState(defaultState.selectedHazardName);
 
-        this.state = {
-            regions: [],
-            regionType: "counties",
-            climatePrediction: [],
-            season: "annual",
-            rcp: "rcp60",
-            year: 2070,
-            loadingPrediction: false,
-            selectedHazardName: "Extreme Storms",
-        };
-    }
+    return (
+        <div className="App">
+            <LCATHeader />
+            <Introduction />
 
-    render() {
-        return (
-            <div className="App">
-                <LCATHeader />
-                <Introduction />
+            <ClimatePredictionLoader
+                regions={regions}
+                season={season}
+                rcp={rcp}
+                regionType={regionType}
+                callback={(prediction) => {
+                    setClimatePrediction(prediction);
+                    setLoadingPrediction(false);
+                }}
+                loadingCallback={() => {
+                    setLoadingPrediction(true);
+                }}
+            />
 
-                <ClimatePredictionLoader
-                    regions={this.state.regions}
-                    season={this.state.season}
-                    rcp={this.state.rcp}
-                    regionType={this.state.regionType}
-                    callback={(prediction) => {
-                        this.setState(() => ({
-                            climatePrediction: prediction,
-                            loadingPrediction: false,
-                        }));
-                    }}
-                    loadingCallback={() => {
-                        this.setState(() => ({
-                            loadingPrediction: true,
-                        }));
+            <div className="white-section">
+                <ClimateMap
+                    regionType={regionType}
+                    regionsCallback={(newRegions, newRegionType) => {
+                        setRegionType(newRegionType);
+                        setRegions(newRegions);
                     }}
                 />
+            </div>
 
-                <div className="white-section">
-                    <ClimateMap
-                        regionType={this.state.regionType}
-                        regionsCallback={(regions, regionType) => {
-                            this.setState({
-                                regionType: regionType,
-                                regions: regions,
-                            });
-                        }}
+            {regions.length > 0 && (
+                <div className="grey-section">
+                    <ClimateSettings
+                        regions={regions}
+                        season={season}
+                        rcp={rcp}
+                        rcpCallback={setRcp}
+                        seasonCallback={setSeason}
+                        yearCallback={setYear}
+                    />
+
+                    <ClimateSummary
+                        climatePrediction={climatePrediction}
+                        year={year}
+                        regions={regions}
+                        loading={loadingPrediction}
+                    />
+
+                    <Graph
+                        regions={regions}
+                        boundary={regionType}
+                        season={season}
+                        rcp={rcp}
+                        seasonCallback={setSeason}
+                        rcpCallback={setRcp}
                     />
                 </div>
+            )}
 
-                {this.state.regions.length > 0 && (
-                    <div className="grey-section">
-                        <ClimateSettings
-                            regions={this.state.regions}
-                            season={this.state.season}
-                            rcp={this.state.rcp}
-                            rcpCallback={(rcp) => {
-                                this.setState(() => ({
-                                    rcp: rcp,
-                                }));
-                            }}
-                            seasonCallback={(season) => {
-                                this.setState(() => ({
-                                    season: season,
-                                }));
-                            }}
-                            yearCallback={(year) => {
-                                this.setState(() => ({
-                                    year: year,
-                                }));
-                            }}
-                        />
+            {regions.length > 0 && (
+                <div className="white-section">
+                    <ClimateHazardRisk loading={loadingPrediction} />
+                </div>
+            )}
 
-                        <ClimateSummary
-                            climatePrediction={this.state.climatePrediction}
-                            year={this.state.year}
-                            regions={this.state.regions}
-                            loading={this.state.loadingPrediction}
-                        />
+            {regions.length > 0 && (
+                <div className="grey-section">
+                    <ClimateImpactSummary
+                        loading={loadingPrediction}
+                        selectedHazardName={selectedHazardName}
+                        hazardCallback={setSelectedHazardName}
+                    />
+                    <KumuImpactPathway
+                        regions={regions}
+                        selectedHazardName={selectedHazardName}
+                        hazardCallback={setSelectedHazardName}
+                    />
+                </div>
+            )}
 
-                        <Graph
-                            regions={this.state.regions}
-                            boundary={this.state.regionType}
-                            season={this.state.season}
-                            rcp={this.state.rcp}
-                            seasonCallback={(season) => {
-                                this.setState(() => ({
-                                    season: season,
-                                }));
-                            }}
-                            rcpCallback={(rcp) => {
-                                this.setState(() => ({
-                                    rcp: rcp,
-                                }));
-                            }}
-                        />
-                    </div>
-                )}
+            {regions.length > 0 && (
+                <div className="white-section">
+                    <PersonalSocialVulnerabilities loading={loadingPrediction} />
+                </div>
+            )}
 
-                {this.state.regions.length > 0 && (
-                    <div className="white-section">
-                        <ClimateHazardRisk loading={this.state.loadingPrediction} />
-                    </div>
-                )}
+            {regions.length > 0 && (
+                <div className="grey-section">
+                    <StaticAdaptations
+                        regions={regions}
+                        selectedHazardName={selectedHazardName}
+                        hazardCallback={setSelectedHazardName}
+                    />
+                </div>
+            )}
 
-                {this.state.regions.length > 0 && (
-                    <div className="grey-section">
-                        <ClimateImpactSummary
-                            loading={this.state.loadingPrediction}
-                            selectedHazardName={this.state.selectedHazardName}
-                            hazardCallback={(hazard) => {
-                                this.setState(() => ({
-                                    selectedHazardName: hazard,
-                                }));
-                            }}
-                        />
-                        <KumuImpactPathway
-                            regions={this.state.regions}
-                            selectedHazardName={this.state.selectedHazardName}
-                            hazardCallback={(hazard) => {
-                                this.setState(() => ({
-                                    selectedHazardName: hazard,
-                                }));
-                            }}
-                        />
-                    </div>
-                )}
-
-                {this.state.regions.length > 0 && (
-                    <div className="white-section">
-                        <PersonalSocialVulnerabilities loading={this.state.loadingPrediction} />
-                    </div>
-                )}
-
-                {this.state.regions.length > 0 && (
-                    <div className="grey-section">
-                        <StaticAdaptations
-                            regions={this.state.regions}
-                            selectedHazardName={this.state.selectedHazardName}
-                            hazardCallback={(hazard) => {
-                                this.setState(() => ({
-                                    selectedHazardName: hazard,
-                                }));
-                            }}
-                        />
-                    </div>
-                )}
-
-                <Feedback />
-
-                <Footer />
-            </div>
-        );
-    }
+            <Feedback />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/utils/defaultState.js
@@ -0,0 +1,10 @@
+export const defaultState = {
+    regions: [],
+    regionType: "counties",
+    climatePrediction: [],
+    season: "annual",
+    rcp: "rcp60",
+    year: 2070,
+    loadingPrediction: false,
+    selectedHazardName: "Extreme Storms",
+};
