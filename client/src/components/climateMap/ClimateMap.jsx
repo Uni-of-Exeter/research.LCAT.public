ab36c1b13df8edf64b9b7d8fd925f751039a4458
Simon Kirby <s.kirby@exeter.ac.uk>
2025-01-16 10:49:04 +0000

    fix: add select all button for some boundaries

--- client/src/components/climateMap/ClimateMap.jsx
+++ client/src/components/climateMap/ClimateMap.jsx
@@ -26,12 +26,31 @@ const tileLayer = {
 const center = [55.8, -3.2];
 const highlightCol = "#ffd768ff";
 
+const mapping = {
+    boundary_uk_counties: "UK Counties",
+    boundary_la_districts: "Local Authority Districts",
+    boundary_lsoa: "LSOA",
+    boundary_msoa: "MSOA (Eng/Wales)",
+    boundary_parishes: "Parishes (Eng/Wales)",
+    boundary_sc_dz: "Data Zones (Scotland)",
+    boundary_ni_dz: "Data Zones (Northern Ireland)",
+    boundary_iom: "Isle of Man",
+};
+
+const regionsToShowToggle = [
+    "MSOA (Eng/Wales)",
+    "Parishes (Eng/Wales)",
+    "Data Zones (Scotland)",
+    "Data Zones (Northern Ireland)",
+];
+
 const ClimateMap = ({ regions, setRegions, allRegions, regionType, setRegionType }) => {
     const [geojsonKey, setGeojsonKey] = useState(0);
     const [geojson, setGeojson] = useState(false);
     const [loading, setLoading] = useState(true);
     const [triggerLoadingIndicator, setTriggerLoadingIndicator] = useState(true);
     const [isDrawerOpen, setIsDrawerOpen] = useState(true);
+    const [selectAllToggleState, setSelectAllToggleState] = useState(false);
     const [searchTerm, setSearchTerm] = useState("");
 
     const layerMap = useRef(new Map());
@@ -102,17 +121,7 @@ const ClimateMap = ({ regions, setRegions, allRegions, regionType, setRegionType
         setTriggerLoadingIndicator(false);
     };
 
-    const regionTypeToName = (type) => {
-        const mapping = {
-            boundary_uk_counties: "UK Counties",
-            boundary_la_districts: "Local Authority Districts",
-            boundary_lsoa: "LSOA",
-            boundary_msoa: "MSOA (Eng/Wales)",
-            boundary_parishes: "Parishes (Eng/Wales)",
-            boundary_sc_dz: "Data Zones (Scotland)",
-            boundary_ni_dz: "Data Zones (Northern Ireland)",
-            boundary_iom: "Isle of Man",
-        };
+    const regionTypeToName = (mapping, type) => {
         return mapping[type] || "";
     };
 
@@ -124,6 +133,27 @@ const ClimateMap = ({ regions, setRegions, allRegions, regionType, setRegionType
 
     const toggleDrawer = () => {
         setIsDrawerOpen(!isDrawerOpen);
+    };
+
+    const handleToggleSelectAll = () => {
+        if (selectAllToggleState) {
+            // Deselect all
+            filteredRegions.forEach((region) => {
+                const isSelected = regions.some((r) => r.id === region.gid);
+                if (isSelected) {
+                    toggleRegion(region.gid, region.name);
+                }
+            });
+        } else {
+            // Select all
+            filteredRegions.forEach((region) => {
+                const isSelected = regions.some((r) => r.id === region.gid);
+                if (!isSelected) {
+                    toggleRegion(region.gid, region.name);
+                }
+            });
+        }
+        setSelectAllToggleState(!selectAllToggleState);
     };
 
     return (
@@ -156,7 +186,7 @@ const ClimateMap = ({ regions, setRegions, allRegions, regionType, setRegionType
                     <LoadingOverlay
                         active={loading && triggerLoadingIndicator}
                         spinner
-                        text={"Loading " + regionTypeToName(regionType)}
+                        text={"Loading " + regionTypeToName(mapping, regionType)}
                     >
                         <MapContainer center={center} zoom={6} minZoom={5} scrollWheelZoom={true}>
                             <GeoJSONLoader
@@ -201,12 +231,19 @@ const ClimateMap = ({ regions, setRegions, allRegions, regionType, setRegionType
                                     );
                                 })}
                             </div>
+                            {filteredRegions.length <= 30 && regionsToShowToggle.includes(mapping[regionType]) && (
+                                <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                                    <button onClick={handleToggleSelectAll}>
+                                        {selectAllToggleState ? "Clear selection" : "Select all"}
+                                    </button>
+                                </div>
+                            )}
                         </div>
                     )}
                 </div>
 
                 <div className="map-selection">
-                    <h2>{regionTypeToName(regionType)} selected</h2>
+                    <h2>{regionTypeToName(mapping, regionType)} selected</h2>
                     {regions.map((r) => (
                         <ul key={r.name}>{r.name}</ul>
                     ))}
