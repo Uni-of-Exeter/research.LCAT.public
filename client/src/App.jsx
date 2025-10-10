59785364f2787714d949a2efce534957c34ccc15
cafoala <catherine.russon@gmail.com>
2025-10-10 16:16:24 +0100

    chore: pr test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -115,6 +115,7 @@ const App = () => {
         <>
             <div className="App">
                 <LCATHeader />
+                <h1>TEST TEST</h1>
                 <Introduction />
 
                 <AllRegionLoader regionType={regionType} setAllRegions={setAllRegions} />
