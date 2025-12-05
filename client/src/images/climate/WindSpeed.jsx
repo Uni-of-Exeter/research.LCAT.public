d5abfb5a02e112be0afaa12922f8e02347faf127
cafoala <catherine.russon@gmail.com>
2025-12-05 14:30:06 +0000

    feat: make icons turn yellow on hover

--- client/src/images/climate/WindSpeed.jsx
+++ client/src/images/climate/WindSpeed.jsx
@@ -1,57 +1,77 @@
-import * as React from "react";
-const SvgWindSpeed = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width={116.629} height={116.629} viewBox="0 0 30.858 30.858" {...props}>
-        <g transform="translate(-146.345 -130.38)">
-            <circle
-                style={{
-                    fill: "#115158",
-                    fillOpacity: 1,
-                    strokeWidth: 0.46499997,
-                }}
-                cx={161.774}
-                cy={145.81}
-                r={15.429}
-            />
-            <g
-                style={{
-                    fill: "#fff",
-                    strokeWidth: 1.35922325,
-                }}
-            >
+import React, { useState } from "react";
+
+const SvgWindSpeed = ({ selected, ...rest }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    // Becomes yellow if hovered OR selected
+    const isActive = selected || isHovered;
+
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={116.629}
+            height={116.629}
+            viewBox="0 0 30.858 30.858"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            {...rest}
+        >
+            <g transform="translate(-146.345 -130.38)">
+                {/* Outer circle stays teal */}
+                <circle
+                    style={{
+                        fill: "#115158",
+                        fillOpacity: 1,
+                        strokeWidth: 0.46499997,
+                    }}
+                    cx={161.774}
+                    cy={145.81}
+                    r={15.429}
+                />
+
+                {/* Inner wind icon that changes colour */}
                 <g
                     style={{
-                        fill: "#fff",
-                        fillRule: "evenodd",
-                        stroke: "none",
+                        fill: isActive ? "#FFD667" : "#fff",
                         strokeWidth: 1.35922325,
                     }}
                 >
                     <g
                         style={{
-                            fill: "#fff",
+                            fill: isActive ? "#FFD667" : "#fff",
+                            fillRule: "evenodd",
+                            stroke: "none",
                             strokeWidth: 1.35922325,
                         }}
                     >
-                        <path
+                        <g
                             style={{
-                                fill: "#fff",
+                                fill: isActive ? "#FFD667" : "#fff",
                                 strokeWidth: 1.35922325,
                             }}
-                            d="M42 24c6.627 0 12-5.373 12-12S48.627 0 42 0 30 5.373 30 12a2 2 0 1 0 4 0 8 8 0 1 1 8 8H4a2 2 0 1 0 0 4z"
-                            transform="matrix(.36048 0 0 .36048 151.23 138.183)"
-                        />
-                        <path
-                            style={{
-                                fill: "#fff",
-                                strokeWidth: 1.35922325,
-                            }}
-                            d="M20 17a7 7 0 1 0-6.441-9.744 2 2 0 0 0 3.68 1.57A3.001 3.001 0 1 1 20 13H2a2 2 0 0 0 0 4zM38 31a6 6 0 1 1-6 6 2 2 0 0 0-4 0c0 5.523 4.477 10 10 10s10-4.477 10-10-4.477-10-10-10H7a2 2 0 1 0 0 4z"
-                            transform="matrix(.36048 0 0 .36048 151.23 138.183)"
-                        />
+                        >
+                            <path
+                                style={{
+                                    fill: isActive ? "#FFD667" : "#fff",
+                                    strokeWidth: 1.35922325,
+                                }}
+                                d="M42 24c6.627 0 12-5.373 12-12S48.627 0 42 0 30 5.373 30 12a2 2 0 1 0 4 0 8 8 0 1 1 8 8H4a2 2 0 1 0 0 4z"
+                                transform="matrix(.36048 0 0 .36048 151.23 138.183)"
+                            />
+                            <path
+                                style={{
+                                    fill: isActive ? "#FFD667" : "#fff",
+                                    strokeWidth: 1.35922325,
+                                }}
+                                d="M20 17a7 7 0 1 0-6.441-9.744 2 2 0 0 0 3.68 1.57A3.001 3.001 0 1 1 20 13H2a2 2 0 0 0 0 4zM38 31a6 6 0 1 1-6 6 2 2 0 0 0-4 0c0 5.523 4.477 10 10 10s10-4.477 10-10-4.477-10-10-10H7a2 2 0 1 0 0 4z"
+                                transform="matrix(.36048 0 0 .36048 151.23 138.183)"
+                            />
+                        </g>
                     </g>
                 </g>
             </g>
-        </g>
-    </svg>
-);
+        </svg>
+    );
+};
+
 export default SvgWindSpeed;
