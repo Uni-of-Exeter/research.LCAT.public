4d3ffeea7998941b70c9a72316b34b5c957d2276
Tom Wilson <t.wilson6@exeter.ac.uk>
2026-08-05 13:59:08 +0100

    feat: add accessibility statement

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,6 +26,7 @@ import ClimateSettings from "./components/climatePrediction/ClimateSettings";
 import ClimateSummary from "./components/climatePrediction/ClimateSummary";
 import Graph from "./components/climatePrediction/Graph";
 import CookieConsent from "./components/cookies/ConsentBanner";
+import AccessibilityStatement from "./components/footer/AccessibilityStatement";
 import Footer from "./components/footer/Footer";
 import LCATHeader from "./components/header/Header";
 import Introduction from "./components/header/Introduction";
@@ -86,6 +87,7 @@ const App = () => {
 
     const [selectedAdaptationHazards, setSelectedAdaptationHazards] = useState([defaultState.selectedImpactHazard]);
     const [filterName, setFilterName] = useState(defaultFilterName);
+    const [isAccessibilityStatementRoute, setIsAccessibilityStatementRoute] = useState(false);
 
     useScrollTracking();
 
@@ -107,6 +109,21 @@ const App = () => {
             setApplyCoastalFilter(defaultState.applyCoastalFilter);
         }
     }, [regions]);
+
+    useEffect(() => {
+        const updateRoute = () => {
+            setIsAccessibilityStatementRoute(window.location.hash === "#accessibility-statement");
+        };
+
+        updateRoute();
+        window.addEventListener("hashchange", updateRoute);
+
+        return () => window.removeEventListener("hashchange", updateRoute);
+    }, []);
+
+    if (isAccessibilityStatementRoute) {
+        return <AccessibilityStatement />;
+    }
 
     return (
         <>
